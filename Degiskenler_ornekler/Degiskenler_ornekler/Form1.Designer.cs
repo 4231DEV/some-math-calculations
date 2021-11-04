544f336d8cba69b66e7149e7fc5694ae7a6658d0
Semih Burhan Sunar <92400578+4231DEV@users.noreply.github.com>
2021-11-04 01:41:05 +0100

    Add files via upload

---
+++ Degiskenler_ornekler/Degiskenler_ornekler/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿
+namespace Degiskenler_ornekler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtcevre = new System.Windows.Forms.TextBox();
+            this.lblsonuccevre = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtmat = new System.Windows.Forms.TextBox();
+            this.txtedeb = new System.Windows.Forms.TextBox();
+            this.txtfızık = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblogrencıson = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtsayi1 = new System.Windows.Forms.TextBox();
+            this.txtsayi2 = new System.Windows.Forms.TextBox();
+            this.txtsayi3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dikdörtgen Alan ve Çevre Hesaplama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uzun kenar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kısa kenar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Sonuç:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(367, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(359, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Çemberin Alan ve Çevresini Hesaplama";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(471, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Yarı Çap:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(612, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 47);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtcevre
+            // 
+            this.txtcevre.Location = new System.Drawing.Point(571, 117);
+            this.txtcevre.Name = "txtcevre";
+            this.txtcevre.Size = new System.Drawing.Size(155, 30);
+            this.txtcevre.TabIndex = 12;
+            // 
+            // lblsonuccevre
+            // 
+            this.lblsonuccevre.AutoSize = true;
+            this.lblsonuccevre.Location = new System.Drawing.Point(367, 187);
+            this.lblsonuccevre.Name = "lblsonuccevre";
+            this.lblsonuccevre.Size = new System.Drawing.Size(75, 25);
+            this.lblsonuccevre.TabIndex = 13;
+            this.lblsonuccevre.Text = "Sonuç:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(367, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(269, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Pi(3,14) olarak hesaplanmıştır";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(847, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Matematik:";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(847, 129);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(94, 25);
+            this.label.TabIndex = 16;
+            this.label.Text = "Edebiyat:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(862, 192);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 25);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Fizik:";
+            // 
+            // txtmat
+            // 
+            this.txtmat.Location = new System.Drawing.Point(972, 67);
+            this.txtmat.Name = "txtmat";
+            this.txtmat.Size = new System.Drawing.Size(100, 30);
+            this.txtmat.TabIndex = 18;
+            // 
+            // txtedeb
+            // 
+            this.txtedeb.Location = new System.Drawing.Point(972, 124);
+            this.txtedeb.Name = "txtedeb";
+            this.txtedeb.Size = new System.Drawing.Size(100, 30);
+            this.txtedeb.TabIndex = 19;
+            // 
+            // txtfızık
+            // 
+            this.txtfızık.Location = new System.Drawing.Point(972, 187);
+            this.txtfızık.Name = "txtfızık";
+            this.txtfızık.Size = new System.Drawing.Size(100, 30);
+            this.txtfızık.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(891, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(196, 25);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Ortalama Hesaplama";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1067, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 46);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Hesapla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblogrencıson
+            // 
+            this.lblogrencıson.AutoSize = true;
+            this.lblogrencıson.Location = new System.Drawing.Point(845, 263);
+            this.lblogrencıson.Name = "lblogrencıson";
+            this.lblogrencıson.Size = new System.Drawing.Size(75, 25);
+            this.lblogrencıson.TabIndex = 23;
+            this.lblogrencıson.Text = "Sonuç:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 25);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "4 işlem hesaplama";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(374, 414);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 25);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Sayı 1:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(374, 474);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(73, 25);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "Sayı 2:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(374, 533);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(73, 25);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "Sayı 3:";
+            // 
+            // txtsayi1
+            // 
+            this.txtsayi1.Location = new System.Drawing.Point(465, 414);
+            this.txtsayi1.Name = "txtsayi1";
+            this.txtsayi1.Size = new System.Drawing.Size(100, 30);
+            this.txtsayi1.TabIndex = 28;
+            // 
+            // txtsayi2
+            // 
+            this.txtsayi2.Location = new System.Drawing.Point(465, 476);
+            this.txtsayi2.Name = "txtsayi2";
+            this.txtsayi2.Size = new System.Drawing.Size(100, 30);
+            this.txtsayi2.TabIndex = 29;
+            // 
+            // txtsayi3
+            // 
+            this.txtsayi3.Location = new System.Drawing.Point(465, 535);
+            this.txtsayi3.Name = "txtsayi3";
+            this.txtsayi3.Size = new System.Drawing.Size(100, 30);
+            this.txtsayi3.TabIndex = 30;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(459, 598);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 47);
+            this.button4.TabIndex = 31;
+            this.button4.Text = "Hesapla";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(1179, 673);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtsayi3);
+            this.Controls.Add(this.txtsayi2);
+            this.Controls.Add(this.txtsayi1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblogrencıson);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtfızık);
+            this.Controls.Add(this.txtedeb);
+            this.Controls.Add(this.txtmat);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblsonuccevre);
+            this.Controls.Add(this.txtcevre);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtcevre;
+        private System.Windows.Forms.Label lblsonuccevre;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtmat;
+        private System.Windows.Forms.TextBox txtedeb;
+        private System.Windows.Forms.TextBox txtfızık;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblogrencıson;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtsayi1;
+        private System.Windows.Forms.TextBox txtsayi2;
+        private System.Windows.Forms.TextBox txtsayi3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Degiskenler_ornekler/Degiskenler_ornekler/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Degiskenler_ornekler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double uzun, kısa,cevre,alan;
+
+            uzun = Convert.ToDouble(textBox1.Text);
+            kısa = Convert.ToDouble(textBox2.Text);
+            cevre = (2 * uzun) + (2 * kısa);
+            alan = uzun * kısa;
+            label5.Text = "Alan:" + alan.ToString() + " Çevre:" + cevre.ToString();
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double yaricap, cevre, alan;
+            yaricap = Convert.ToDouble(txtcevre.Text);
+            cevre = 2 * yaricap * 3.14;
+            alan = yaricap * yaricap * 3.14;
+            lblsonuccevre.Text = "Alan:" + alan.ToString() + " Çevre:" + cevre.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double mat, edeb, fızık, ort;
+            mat = Convert.ToDouble(txtmat.Text);
+            edeb = Convert.ToDouble(txtedeb.Text);
+            fızık = Convert.ToDouble(txtfızık.Text);
+            ort = (mat + edeb + fızık) / 3;
+            lblogrencıson.Text = "Ortalama: " + ort.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, sayi3, toplam, bölüm, fark, çarpım;
+            sayi1 = Convert.ToDouble(txtsayi1.Text);
+            sayi2 = Convert.ToDouble(txtsayi2.Text);
+            sayi3 = Convert.ToDouble(txtsayi3.Text);
+            toplam = sayi1 + sayi2 + sayi3;
+            fark = sayi1 - sayi2 - sayi3;
+            çarpım = sayi1 * sayi2 * sayi3;
+            bölüm = sayi1 / sayi2 / sayi3;
+            MessageBox.Show("toplam:" + toplam + "\n" + "fark:" + fark + "\n" + "çarpım:" + çarpım + "\n" + "bölüm:" + bölüm);
+
+
+        }
+    }
+}
